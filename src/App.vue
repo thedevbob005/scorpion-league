5696620d9b47afb42cc3b559b6aca820689259c3
Sayantan Bakshi <thedevbob005@gmail.com>
2018-10-28 10:45:48 +0530

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,59 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
+  <v-app dark>
+    <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            right
+            app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
 
+      <v-spacer></v-spacer>
+
+      <v-btn icon class="hidden-sm-and-down">
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon class="hidden-sm-and-down">
+        <v-icon>apps</v-icon>
+      </v-btn>
+
+      <v-btn icon class="hidden-sm-and-down">
+        <v-icon>refresh</v-icon>
+      </v-btn>
+
+      <v-btn icon class="hidden-sm-and-down">
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+    </v-toolbar>
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -28,7 +63,7 @@ export default {
   name: 'App',
   data () {
     return {
-      //
+      drawer: false
     }
   }
 }
